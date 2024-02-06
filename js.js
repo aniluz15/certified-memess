aeb3da1d5a84f64a41127cc752e37e485d41a456
Anissa Molina <ani_luz15@yahoo.com>
2024-02-06 10:41:39 -0800

    blehhhh
    i no understand js

---
+++ js.js
@@ -0,0 +1,11 @@
+let certificateoptions = {choice:"burger", choice: "green", choice: "skill", choice:"noggin"};
+
+function submitButton(){
+    console.log("e")
+}
+// document.getElementById("Submit").addEventListener(onclick, submitButton);
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("Submit").addEventListener("click", submitButton);
+
+})
